922bc8ef6ec7a1b81e1c7bfd2fe1227952b7e89b
Tai Henrichs <tai.henrichs@gmail.com>
2020-11-27 21:30:57 -0600

    Completed Class Conversion

--- code/generate.py
+++ code/generate.py
@@ -3,10 +3,11 @@ import numpy as np
 import collections
 from collections import Counter
 
-def topListSample(n,k_distribution,theta=None,phi=None,s0=None):
-    """This function generates a single sample according
-    to Mallows Models adapted to top-k rankings given a parameter of dispersion
-    (theta or phi), where the values of k are controlled by k_distribution
+class MallowsSample:
+    """This class represents a single sample generated from a
+    Mallows Models adapted to top-k rankings given a parameter of dispersion
+    (theta or phi), and where the lengths and quantities of the top-lists are
+    dictated by k_distribution.
         Parameters
         ----------
         n: int
@@ -24,20 +25,57 @@ def topListSample(n,k_distribution,theta=None,phi=None,s0=None):
             The dispersion parameter phi
         s0: ndarray
             The consensus ranking. The identity ranking by default.
-        Returns
-        -------
-        ndarray
-            The top-lists generated
     """
-    return np.array([rank[:k] for
-        rank in mk.sampling_top_k_rankings(freq, n, k, theta, phi, s0) for
-        k, freq in k_distribution])
+    def ___init___(self,n,k_distribution,theta=None,phi=None,s0=None):
+        self.n = n
+        self.theta = theta
+        self.phi = phi
+        self.s0 = s0
+        self.sample = topListSample(self,n,k_distribution,theta,phi,s0)
 
-def topKSample(m,n,k,theta=None,phi=None,s0=None):
-    """This function generates m top-k rankings according according
-    to Mallows Models adapted to top-k rankings given a parameter of dispersion
-    (theta or phi). This function differs from mk.sampling_top_k_rankings
-    only in that none of the entries in the top-lists will be NaN.
+    sampleType = "Mallows"
+
+    def topListSample(self,n,k_distribution,theta=None,phi=None,s0=None):
+        """This function generates a single sample according
+        to Mallows Models adapted to top-k rankings given a parameter of dispersion
+        (theta or phi), where the values of k are controlled by k_distribution
+            Parameters
+            ----------
+            n: int
+                The number of candidates considered by rankers. Note that
+                k <= n, since rankers cannot create preference lists that
+                rank more candidates than exist.
+            k_distribution: dict [int : int]
+                The provided dictionary indicates the quantity of
+                lists in the returned sample of each length. A given
+                [key : value] entry dictates that value top-key-lists will
+                be created in the resulting sample.
+            theta: float, optional (if phi given)
+                The dispersion parameter theta
+            phi: float, optional (if theta given)
+                The dispersion parameter phi
+            s0: ndarray
+                The consensus ranking. The identity ranking by default.
+            Returns
+            -------
+            ndarray
+                The top-lists generated
+        """
+        return np.array([ ranking[ranking is not np.nan] for
+            ranking in mk.sampling_top_k_rankings(freq, n, k, theta, phi, s0) for
+            k, freq in k_distribution])
+
+    def label(self):
+        return f"{sampleType}_candidates-{n}_theta-{theta}_phi-{phi}"
+
+    def __str__(self):
+        return label(self) + "\n" + "Rankings: " + np.array_str(sample) + "\n" +
+                "Consensus Ranking: " + np.array_str(s0)
+
+class MallowsSampleTopK(MallowsSample):
+      """This class represents a sample of m top-k-lists sampled from a
+        Mallows Models adapted to top-k rankings given a parameter of dispersion
+        (theta or phi).
         Parameters
         ----------
         n: int
@@ -59,72 +97,83 @@ def topKSample(m,n,k,theta=None,phi=None,s0=None):
         ndarray
             The top-lists generated
     """
-    return topListSample(n,{k : m},theta,phi,s0)
+    def ___init___(self,m,n,k,theta=None,phi=None,s0=None):
+        self.m = m
+        self.k = k
+        super().___init___(n, {k : m},theta,phi,s0)
 
-def poissonSample(m, lower = 0, upper = float('inf'), lda):
-    """Returns m values drawn from a Poisson distribution on lambda for
-        which all values are in [lower, upper] through repeated sampling.
-        Not guaranteed to terminate.
+    def label(self):
+        return super().label() + f"_K-{K}"
+
+    sampleType = "Mallows_Top-K"
+
+class MallowsSamplePoisson(MallowsSample):
+    """This class represents a single sample generated from
+        a Mallows Models adapted to top-k rankings given a parameter of dispersion
+        (theta or phi). The lengths of rankers' top-lists are sampled from a
+        Poisson distribution on lda. With m top-lists, we define one m_i for
+        each i in [1,m], where each m_i is randomly sampled from a truncated
+        Poisson distribution with the interval [1,n].
+        Then, we have a top-m_i-list in the sample for
+        each m_i, for m top-lists in total.
+
         Parameters
         ----------
         m: int
-            The number of elements in the requested sample
-        lower: int
-            Lowerbound of the sampled values
-        upper: int
-            Upperbound of the sampled values.
+            The number of rankings to generate.
+        n: int
+            The number of candidates considered by rankers.
         lda: float
-            The lambda parameter on which the Poisson distribution is
-            defined.
+            The parameter on which the Poisson distribution is defined, short
+            for lambda.
+            If lda is an integer, the median number of candidates
+            ranked by voters will be lda. lda is always the expected value of
+            an element randomly sampled from a Poisson distribution on lda.
+        theta: list of floats, optional (if phi given)
+            The dispersion parameter theta
+        phi: list of floats, optional (if theta given)
+            The dispersion parameter phi
+        s0: ndarray
+            The consensus ranking. The identity ranking by default.
     """
-    rng = np.random.default_rng()
-    # Samples are drawn repeatedly until enough values
-    # reside in [lower,upper]
-    while True:
-        s = rng.poisson(lda, m)
-        s = np.where(lower <= s <= upper)
-        if len(s) >= n:
-            return s[:n]
-        # Double the number of values sampled each time to
-        # increase the chance that sufficient values lay in [lower,upper]
-        m *= 2
+    def ___init___(self,m,n,lda,theta=None,phi=None,s0=None):
+        self.m = m
+        self.lda=lda
+        super().___init___(n, Counter(poissonSample(m, 1, n, lda)),theta,phi,s0)
 
-def sampleFromPoisson(m,n,lda,thetas=None,phis=None,s0=None):
-"""This function generates a single sample according
-    to Mallows Models adapted to top-k rankings given a parameter of dispersion
-    (theta or phi). The lengths of rankers' top-lists are sampled a from a
-    Poisson distribution on lda. The trunctation ensures all
-    voters rank at least one candidate, and no voter ranks more than n
-    candidates. Formally, with m lists, we define one m_i for
-    each i in [1,m], where each m_i is randomly sampled from a truncated
-    Poisson distribution. Then, we have a top-m_i-list in the sample for
-    each m_i.
+    sampleType = "Mallows_Poisson"
 
-    Parameters
-    ----------
-    m: int
-        The number of rankings to generate.
-    n: int
-        The number of candidates considered by rankers.
-    lda: float
-        The parameter on which the Poisson distribution is defined, short
-        for lambda.
-        If lda is an integer, the median number of candidates
-        ranked by voters will be lda. lda is always the expected value of
-        an element randomly sampled from a Poisson distribution on lda.
-    theta: list of floats, optional (if phi given)
-        The dispersion parameter theta
-    phi: list of floats, optional (if theta given)
-        The dispersion parameter phi
-    s0: ndarray
-        The consensus ranking. The identity ranking by default.
-    Returns
-    -------
-    ndarray
-        List of samples generated
-"""
-k_distribution = Counter(poissonSample(m, 1, n, lda))
-return topListSamples(n,k_distribution,thetas,phis,s0)
+    def label(self):
+        return super().label() + f"_lambda-{lda}"
+
+    def poissonSample(m, lower = 0, upper = float('inf'), lda):
+        """Returns m values drawn from a Poisson distribution on lambda for
+            which all values are in [lower, upper] through repeated sampling.
+            Not guaranteed to terminate.
+            Parameters
+            ----------
+            m: int
+                The number of elements in the requested sample
+            lower: int
+                Lowerbound of the sampled values
+            upper: int
+                Upperbound of the sampled values.
+            lda: float
+                The lambda parameter on which the Poisson distribution is
+                defined.
+        """
+        rng = np.random.default_rng()
+        # Samples are drawn repeatedly until enough values
+        # reside in [lower,upper]
+        while True:
+            s = rng.poisson(lda, m)
+            s = arr[np.where(lower <= s <= upper)]
+            if len(s) >= n:
+                return s[:n]
+            # Double the number of values sampled each time to
+            # increase the chance that sufficient values lay in [lower,upper]
+            m *= 2
+
 
 # Main function generates synthetic data-sets using the Mallows-Model
 # and saves the output in CSV format. The data-sets are made for the
