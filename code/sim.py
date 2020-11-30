691e938aa1fdbe4240cfca1bb1e59f7662565860
ammareltigani <ammartigani12@gmail.com>
2020-11-30 12:55:38 -0500

    parseCSV and writeToFile remaining

--- code/sim.py
+++ code/sim.py
@@ -9,7 +9,7 @@ from generate import MallowsSampleTopK
 
 """
 This is the class in charge of taking arguments from user and calling
-appropriate funcitons.
+appropriate functions.
 ------------------------------
 
 The different choices that we provide are:
@@ -31,6 +31,8 @@ The different choices that we provide are:
             i'th candiate (int) and the second is a (str) describes the candiate
             iii. The n+2'nd row must have as its first column the total number
             of voters N (size of the input)
+            iv. the top-lists being at line n+3, are newline separated from each other, and the
+            alternatives within each list are comman separated and ordered from left to right
 
     2. What algorithm(s) the user would like to use for top-list rank aggregation:
         a. 'FootRule+'
@@ -42,46 +44,92 @@ The different choices that we provide are:
 
     -------------------------------
 
-Usage: python3  sim.py  <s [OR] r>  <[if s] n,N,theta,k>  <[if s] s0=None>
-       <[if r]: path/to/file.CSV>  <algo1, algo2=None, ...>
+usage: python3 sim.py <s [OR] r>  <algo1,algo2=None, ...>  <[if s] n,N,theta,k>  <[if s] s0>  <[if r]: path/to/file.CSV>
 
 Examples:
-    python3 sim.py s [10,100,0.5,3] [8,4,6,1,2,9,3,7,5,10] [Score-Then-Borda]
+    python3 sim.py [Score-Then-Borda] s [10,100,0.5,3] [8,4,6,1,2,9,3,7,5,10] 
 
-    python3 sim.py s [10,100,2,4] [RandomSort, Borda+, FootRule+]
+    python3 sim.py [RandomSort,Borda+,FootRule+] s [10,100,2,4] 
 
-    python3 sim.py r data/soi/ED-00001-00000001.CSV [Score-Then-Adjust]
+    python3 sim.py [Score-Then-Adjust] r data/soi/ED-00001-00000001.CSV 
 
 """
 
 class Simulaton:
 
     def __init__(self):
-        self.results = list()
-        self.algorithms = list()
+        """
+        Instance variables:
+            'results' - a list of 3-tuples (algorithm, distance, time) that saves 
+            performance info of each algorithm
 
-        self.funcDict = {"FootRule+": footrule.run, "RandomSort": randomsort.run,\
-        "Borda+": borda.run, "Score-Then-Borda+": scorethenborda.run, \
-        "Score-Then-PTAS": scorethenptas.run, "Score-Then-Adjust": scorethenadjust.run}
+            'algorithms' - list of function names (str)  to execute top list aggregation 
+            on based on args
+
+            'funcDict' - python dictionary mapping function name to function call from imports
+
+            'data' - an object of type Counter in which input lists are stored
+            with their repective frequencies. Each top-list (tuple) is the key and the frequency
+            of such list is the value (int) 
+
+            'params' - dataset information to keep track of like n, N, k, and s0
+            Note:
+                n, N, k are type (int) 
+                s0 is a list of type (int)
+                theta is type (float)
+
+
+        """
+        self.results = []
+
+        self.algorithms = []
+
+        self.funcDict = {
+                "FootRule+": footrule.run, 
+                "RandomSort": randomsort.run,
+                "Borda+": borda.run, 
+                "Score-Then-Borda+": scorethenborda.run, 
+                "Score-Then-PTAS": scorethenptas.run, 
+                "Score-Then-Adjust": scorethenadjust.run
+                }
 
         self.data = None
-        self.params = None
+
+        self.params = {
+                'n': None,
+                'N': None,
+                'k': None,
+                'theta': None,
+                's0': None
+                }
 
 
 
     def writeToFile(self):
-        pass
+        """
+        Creates appropriate files for each algorithms (if doesn't exists already)
+        and appends the comma separated string '<distance>, <time>' as a line
+        
+        """
+        for c in self.results:
+            label = c[0]
+            if self.synth:
+                label.append(F"{self.params['n']}, {self.params['N'], {self.params['theta']}}"
+            f = open(label, "a+")  # append and read mode 
+            f.write(F"{c[1]}, {c[2]} \n") 
+            f.close()
 
+            #TODO: pick up from here
 
 
     def genMallows(self, params):
         """
         This method returns a object of type Counter in which input lists are stored
-        with their repective frequencies. Each list (tuple) is the key and the frequency
+        with their repective frequencies. Each top-list (tuple) is the key and the frequency
         of such list is the value (int)
         """
         return MallowsSamplePoisson(params).sample
-        #return MallowsSampleTopK(params).sample
+        #return MallowsSampleTopK(params).sample         #manually switch between Poisson and TopK
 
 
 
@@ -100,99 +148,93 @@ class Simulaton:
 
 
 
-
     def handleFunc(self):
         """
-        This method accesses runs all the algorithms specifies in the list 'self.algorithms'
-        for each sampled dataset in self.data (numpy array).
+        This method runs all the algorithms specifies in the list 'self.algorithms'
+        for stored in self.data 
 
-        After each iteration, self.results is updated by running self.updateResults()
+        After each iteration, results are updated by receiving a 3-tuple 
+            (<algorithm name>, <kendall tau distance>, <time recorded>)
+        from particular algorithm in 'alg'
+
         """
         for func in self.algorithms:
             if func not in self.funcDict:
                 print("incorrect function name! {} was not found" .format(fun))
             alg = self.funcDict[func]
-            self.updateResults(alg(self.data))
+
+            #passes Counter object dataset as well as data specs
+            self.results.append(alg(self.data, self.params)) 
 
 
 
 
-    def updateResults(self, output= {}):
-        pass
+    def __parseListArg__(self, s):
+        """
+        This is a helper for main
+        """
+        # remove "[" and "]"
+        sp = s[1:-1]
 
+        # split at every comma and save in list
+        l = sp.split(",")
 
+        #if parsing numerical list, convert to list of (int)
+        if l[0].isnumeric():
+            return [int(i) for i in l]
 
+        #else leave as list of (str) case: parsing algorithms list
+        return l
 
 
     def main(self, args):
         """
-        Thi method takes in list of args and then calls genMallows() or parseCSV()
-        to populate self.data. It then calls handleFunc() and writeToFile()
+        This method takes in list of args and then calls genMallows() or parseCSV()
+        to populate self.data depending on args. It uses the helper function parseListArg
+        process comma separated arguments (instead of space separated) into a list 
 
-        Params:
-        --------------
-        args: a lit of args as defined by <s [OR] r>  <[if s] n,N,theta,k>
-              <[if s] s0=None>  <[if r]: path/to/file.CSV>  <algo1, algo2=None, ...>
+        Finally, it calls handleFunc() and writeToFile()
 
         """
         arglen = len(args)
 
-        if args[0] == "r":
-            self.data = self.parseCSV(args[1])
+        self.algorithms = self.parseListArg(args[0])
 
-        else:
-            #remove "[" and "]"
-            params = args[1]
-            params = params[1:-1]
-            params = params.split(",")
+        if args[1] == "r":
+            self.data = self.parseCSV(args[2])
+
+        elif args[1] == "s":
+            params = self.parseListArg(args[2])
+
+            # if 'k' is not specified
             if len(params) == 3:
-                #default k is n/2 if user doesn't specify
-                params.append(params[1] // 2)
+                # default k is n/2 if user doesn't specify
+                self.params['k'] = params[1] // 2
 
-            #we don't need to worry about ground ranking, not specified by user
-            if arglen == 3:
-                self.data = self.genMallows(params)
+            # update params dict from parsed list
+            self.params['n'] = params[0]
+            self.params['N'] = params[1]
+            self.params['theta'] = params[2]
 
-            #extract ground ranking and make it part of params for data generation
-            else arglen == 4:
-                #remove "[" and "]"
-                s0 = args[2]
-                s0 = s0[1:-1]
-                s0 = s0.split(",")
-                params.append(s0)
-                self.data = self.genMallows(params)
+            # extract user specified ground ranking if exists
+            if arglen == 4:
+                self.params['s0'] = s0self.parseListArg(args[3])
 
-            #update self.params for later use by other methods
-            self.params = params
+            # generate data
+            self.data = self.genMallows(self.params)
 
 
-        if args[2] == None:
-            print("wrong usage! arg[2] is null")
-            return
-        #remove "[" and "]"
-        algos = args[2]
-        algos = algos[1:-1]
-        #update self.algorithms fir later use by other methods
-        self.algorithms = algos.split(",")
-
-
-        self.handleFunc()   #runs all algorithms
-        self.writeToFile()  #writes results to file'
-
-        ## TODO: Method is broken! Fix!!!
-
+        self.handleFunc()   # runs all algorithms
+        self.writeToFile()  # writes results to files
 
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("wrong usage. Please do: python3  sim.py  <s [OR] r>  <[if s] n,N,\
-        theta,k>  <[if s] s0=None>  <[if r]: path/to/file.CSV>  <algo1,algo2=None\
-        , ...>")
+    if len(sys.argv) < 3 or len(sys.argv) > 5:
+        print("wrong usage. Please do: python3 sim.py <s [OR] r>  <algo1,algo2=None, ...>  <[if s] n,N,theta,k>  <[if s] s0=None>  <[if r]: path/to/file.CSV>")
     else:
         sim = Simulation()
         sim.main(sys.argv[1:])
-        sim.writeToFile()
         print(sim)
 
     print("\n Done!")
