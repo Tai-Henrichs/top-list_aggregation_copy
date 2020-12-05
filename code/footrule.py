603cb2ff687796e132105b7b63b053775c3bde35
ammareltigani <ammartigani12@gmail.com>
2020-12-04 21:08:31 -0500

    base for footrule+ and utils started, testing left. added gitignore

---
+++ code/footrule.py
@@ -0,0 +1,118 @@
+import utils
+import numpy as np
+
+from scipy.optimize import linear_sum_assignment
+
+
+ALGORITHM_NAME = "FootRule+"
+
+def run(data, params):
+    """
+    This method implements the foorule+ algorithm by considering
+    the input data and params to create an implicit bipartite graph
+    representation and finding assigning each alternative to some
+    rank in the final full rank sigma.
+    -------------------------------------
+
+    Params
+
+    'data': Counter object 
+            The keys  in this Counter are tuple top-lists and the 
+            values are the mulitiplicities of each top-list. Both 
+            the elements in the tuples and the values are ints
+
+    'params': dict
+              A python dictionary with that holds statics and info
+              on the dataset stored on 'data'. Some of the keys are
+              'n', 'N', 'k', and 'theta'. Refer to sim.py for full
+              documentation.
+    ------------------------------------
+
+    Returns 
+
+    'AGORITHM_NAME': str
+                     The identifier for the algorithm implemented in
+                     this current file.
+
+    'time': float
+            Time if took for the main component of the algorithm to run
+
+    'acccuracy': float
+                 The generalized Kendall Tau Distance between the 
+                 dataset top-lists and sigma
+
+    """
+    # start the timer
+    start_time = time.clock()
+
+    # 'n' is the number of candidates, also the number of ranks
+    n = params['n']
+    # 'N' is the total number of voters
+    N = params['N']
+    # 's0' is the optional ground truth full ranking of the candidates
+    # (distribution is drawn off this full ranking)
+    s0 = params['s0']
+
+    # get the n x n np.array cost matrix by calling helper function
+    # rows are the candidates and cols are the positions
+    cost = createCostMatrix(data, n, m)
+
+    # run optimization algorithm from scipy library
+    _, ranking = linear_sum_assignment(cost)
+    # convert answer from np.array to tuple format
+
+    # TODO:correction factor because we use zero-indexing in createCostMatrix
+    off_by_one = np.ones(np.shape(ranking))
+    sigma = tuple(ranking + off_by_one)
+
+    # time separate line because too ambiguous when it stops if put in rtn
+    time_elapsed = time.clock()-start_time
+
+    #print("{ALGORITHM_NAME}: {sigma}\n")
+    return ALGORITHM_NAME, time_elapsed, utils.generalizedKendallTauDistance(data, sigma, n, N, s0)
+
+
+
+def createCostMatrix(data, n, N):
+    """
+    This function creates a cost matrix based on the input top-lists and
+    the rule: C(i, j) := sum_{r=1}^j  (j −r) · p(πi = r)
+    --------------------------------------
+
+    Params
+
+    'data': Counter object 
+            The keys  in this Counter are tuple top-lists and the 
+            values are the mulitiplicities of each top-list. Both 
+            the elements in the tuples and the values are ints
+
+    'n': int
+         The number of candidates, which is also the number of ranks
+
+    'N': int
+         The total number of voters
+    ---------------------------------------
+
+    Returns 
+        'arr': 2D n x n np.array
+               The cost matrix with candidates for the rows and ranks
+               for the columns
+        
+    """
+    # gets n by n matrix of occurances for each alternative for each rank
+    p = utils.alternativeRankFrequency(data,n,N)
+    
+    # creates empty n by n cost matrix
+    # should not be any empty entries by end of function
+    arr = np.empty((n,n))
+
+    # follows rule above and assigns cost C(i,j)
+    for i in range(n):          # for each alternative
+        for j in range(n):      # for each rank
+            summ = 0                # start sum
+            for r in range(j):
+                summ += (j-r) * p[i, r]     # following algorithm formula
+            arr[i,j] = summ
+
+    return arr
+
---
+++ code/utils.py
@@ -0,0 +1,119 @@
+import numpy as np
+from mallows_kendall import kendall_tau
+def generalizedKendallTauDistance(data, sigma, n, N, s0=None):
+    """
+    This method computes the average Kendall Tau Distance by computing
+    the generalized Kendall Tau Distance between each top-list in data 
+    and sigma, then dividing the total by N.
+    Before making comparisons between some top-list pi_i and sigma, we
+    convert pi_i to the full ranking tau_i which is just pi_i but takes
+    on sigma's candidates ordering for all candidates that are tied in
+    pi_i (i.e. candidates that are not ranked by pi_i)
+
+    --------------------------------
+
+    Params
+
+    'data': Counter object 
+            The keys  in this Counter are tuple top-lists and the 
+            values are the mulitiplicities of each top-list. Both 
+            the elements in the tuples and the values are ints
+
+    'sigma': int tuple
+             A single full ranking returned by some algorithm
+             being tested
+
+    'n': int
+         Number of alternatives/candidates. Also the number of ranks
+         in a full ranking context
+
+    'N': int
+         The total number of voters in this instance dataset
+
+    's0': int tuple
+          The true ground ranking of a given instance dataset
+          taken from some distribution (Mallows Model). We additionaly
+          compute the regular KT distance between sigma and s0 to
+          additionally compare how close different algorithms are to
+          a general consensus.
+    --------------------------------
+
+    Returns
+
+        'cost': float
+                The generalized Kendall Tau Distance as defined by the 
+                Simon and Mathieu paper 'How To Rank Top-Lists'
+
+        'ground_dist': float [OPTIONAL]
+                       Another reasonable measure of accuracy: the distance
+                       between the approximate solution sigma and the ground
+                       truth (known optimal ranking s0). This is useful in
+                       cases where common prior are reasonable assumptions and
+                       has many application beyond voting theory
+
+    """
+    def piToTau(pi):
+        # set of all the candidate IDs
+        complete = {i for i in range(1,n+1)}
+
+        # gets the symmetri difference, i.e the candidates in complete (full
+        # rankging that are not in pi (because they were not ranked)
+        ties = complete.symmetric_difference(pi)
+        # this list will contain all the tied elements in the order of sigma
+        extension = []
+        for e in sigma:
+            if e in ties:
+                extension.append(e)
+
+        # return the extended list pi + ties ordered based on sigma --> tau
+        return tuple(list(pi) + extension)
+
+    # sum_{i=1}^N K(sigma, tau_i) / N
+    cost = 0
+    for x in data:
+        tau_x = piToTau(x)
+        cost += kendall_tau(sigma, x) * data[x]
+
+    return cost / N
+
+def alternativeRankFrequency(data, n, N):
+    """
+    This functions computes an n by n matrix 'p' where p[i,j] is the number of
+    voters that placed candidate i in rank k.
+    --------------------------------------
+
+    Params
+
+    'data': Counter object 
+            The keys  in this Counter are tuple top-lists and the 
+            values are the mulitiplicities of each top-list. Both 
+            the elements in the tuples and the values are ints
+
+    'n': int
+         The number of candidates, which is also the number of ranks
+    ---------------------------------------
+
+    Returns 
+
+        'p': 2D n x n np.array
+             Matrix describing the frequency of each alternative appearing in a
+             given rank position
+    """
+
+    # initialize n by n array for occurence of each alternatives on each rank
+    # the rows are the alternatives and the columns are the ranks
+    # note: if no candidate ever appears in some rank, then then stays 0
+    p = np.zeros((n,n))
+
+    # loop over all the keys (top-lists) in data
+    for x in data:
+        # loop over each alternative index in a given top-list
+        for i in range(len(x)):
+            # get alternative ID (and decrement for zero-index matrix)
+            alternative = x[i] - 1
+            # make sure to increment by number of multiplicities of current top-list
+            p[alternative, i] += data[x]
+
+    # remark: p is zero indexed for rankings and alternatives. The necessary change of
+    # decrementing all alternative IDs by one was made.
+    return p / N
